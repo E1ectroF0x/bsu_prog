d48455a8f6c0f7f7632c5864d15c7e8c362bf7f1
Ilya <agronat88@icloud.com>
2019-12-10 16:20:46 +0300

    add profieTableViewControllers

--- tableSettigins/tableSettigins/PageViewController.swift
+++ tableSettigins/tableSettigins/PageViewController.swift
@@ -10,34 +10,29 @@ import UIKit
 
 class PageViewController: UIPageViewController {
     
-    let infoTableView = ProfileTableViewController(index: 0)
+    let infoTableView = InfoPageTableViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
         setViewControllers([infoTableView], direction: .forward, animated: false, completion: nil)
     }
     
-}
-
-extension PageViewController: UIPageViewControllerDataSource {
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        let index = (viewController as! ProfileTableViewController).index - 1
-        if index < 0 {
-            return nil
+    func switchTableVC(index: Int) -> UITableViewController{
+        switch index {
+        case 0: return InfoPageTableViewController()
+        case 1: return CreditTableViewController()
+        case 2: return StudyProgressTableViewController()
+       
+            
+        default: return UITableViewController()
+            
         }
-        return ProfileTableViewController(index: index)
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        let index = (viewController as! ProfileTableViewController).index + 1
-        if index > 2 {
-            return nil
-        }
-        return ProfileTableViewController(index: index)
     }
     
     func nextPageWithIndex(index: Int){
-        setViewControllers([ProfileTableViewController(index: index)], direction: .forward, animated: false, completion: nil)
+       let controller = switchTableVC(index: index)
+           setViewControllers([controller], direction: .forward, animated: false, completion: nil)
 
-    }
+       }
     
 }
+
---
+++ tableSettigins/tableSettigins/ProfileTableVC/CreditTableViewController.swift
@@ -0,0 +1,52 @@
+//
+//  CreditTableViewController.swift
+//  tableSettigins
+//
+//  Created by Li on 12/10/19.
+//  Copyright © 2019 Li. All rights reserved.
+//
+
+import UIKit
+
+class CreditTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return Constants.CREDIT_NAME_CELL.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = Constants.CREDIT_NAME_CELL[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        switch indexPath.row {
+        case 0...2:
+            let message = """
+      К оплате: 30.7 руб
+    Оплата до: 25.16.2019
+    """
+            let alert = UIAlertController(title: Constants.CREDIT_NAME_CELL[indexPath.row], message: message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        default:
+            break
+            //present(InfoPageTableViewController(index: 0), animated: true, completion: nil)
+        }
+    }
+    
+}
+
+
