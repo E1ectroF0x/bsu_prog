5851a03c5d8b727fbbfabbb7432b4a7115606054
Nikita Markov <foxelectro1@gmail.com>
2020-01-27 22:52:37 +0300

    Rework teacher logic for right TeacherDTO

--- angular/src/app/modules/teacher/components/table/teacher-table.component.ts
+++ angular/src/app/modules/teacher/components/table/teacher-table.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Teacher} from '../../models/teacher.model';
+import {TeacherService} from '../../../../services/teacher.service';
+import {Subscription} from 'rxjs';
 
 
 @Component({
@@ -7,10 +9,22 @@ import {Teacher} from '../../models/teacher.model';
   templateUrl: 'teacher-table.component.html',
   styleUrls: ['./teacher-table.component.css']
 })
-export class TeacherTableComponent {
+export class TeacherTableComponent implements OnInit, OnDestroy {
 
-  @Input() public _teachers: Teacher[];
+  public _teachers: Teacher[];
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private teacherService: TeacherService) { }
+
+  ngOnInit(): void {
+      this.subscriptions.push(this.teacherService.getAll().subscribe(teachers => {
+        this._teachers = teachers;
+        console.log("Teachers loaded successfully");
+      }))
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
 
 }
