f9ee77094126e5343c75df6e2193d3a9a76c0bbc
Дмитрий Катющенко <48712059+imudak1@users.noreply.github.com>
2020-02-15 23:59:37 +0300

    poxer

--- restapi/src/main/java/com/bsu/edu/restapi/controller/UserController.java
+++ restapi/src/main/java/com/bsu/edu/restapi/controller/UserController.java
@@ -3,6 +3,9 @@ package com.bsu.edu.restapi.controller;
 import com.bsu.edu.restapi.entity.User;
 import com.bsu.edu.restapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +25,14 @@ public class UserController {
     @GetMapping(value = "/all")
     public List<User> getAll() {
         return userService.getAllUsers();
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/current")
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // todo exclude password from model!
+        return userService.getUserByUsername(((org.springframework.security.core.userdetails.User) authentication.getPrincipal()).getUsername());
     }
 
 }
