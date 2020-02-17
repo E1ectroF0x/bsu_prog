77386af496a91d3be3d5d905f8e3dc4a486da00f
GetM0ney <vova9.11.vova@icloud.com>
2020-02-17 09:20:55 +0300

    delete constants

--- tableSettigins/tableSettigins/Constants.swift
+++ tableSettigins/tableSettigins/Constants.swift
@@ -26,7 +26,7 @@ struct Constants {
                                       "Форма обучения", "Почта",
                                       "Адресс проживания"]
     static let CREDIT_NAME_CELL = ["Оплата за обучение", "Пересдачи", "За общежитие", "История платежей"]
-    static let INFO_TABLE_INFO = ["faculty", "Компьютерная безопастность", "2 курс, 7 группа", "Дневная, бюджет", "rct.truhov@bsu.by", "+375259612924", "Курчатова 8, Б84б"]
+   
     
     
     static let customBlue = UIColor(red: 20.0 / 255.0, green: 78.0 / 255.0, blue: 157.0 / 255.0, alpha: 1.0)
@@ -37,27 +37,5 @@ struct Constants {
                                  "13:50 - 15:10",
                                  "15:20 - 16:40",
                                  "прокоповичтайм",]
-    static let currentDayType = ["Практическое занятие", "Практическое занятие"]
-    
-    static let currentDayLesson = ["Теоритическая физика", "Методы мат.физики"]
-    
-    static let currentDayName = ["Жилко Виталий Владимирович", "Егоров Андрей Александрович"]
-    
-    static let currentDayLocation = ["каб.321","каб.613"]
-    
-    static let lessonsNumber = 2
-    
-    static let otherDayType = ["Практическое занятие", "Лабораторные занятия", "Лабораторные занятия"]
-    
-    static let otherDayLesson = ["Общая физика", "Общая физика", "Общая физика"]
-    
-    static let otherDayName = ["Трухан Сергей Васильевич", "Григорчук Елена Сергеевна", "Григорчук Елена Сергеевна"]
-    
-    static let otherDayLocation = ["каб.613","каб.513", "каб.513"]
-    
-    static let otherLessonsNumber = 3
-    
-    
-    
     
 }
